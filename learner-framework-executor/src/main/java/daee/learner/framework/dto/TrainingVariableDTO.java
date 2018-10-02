d0f0b5c1c47aa07cfffb85a26811311297e5015f
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-10-02 15:26:13 -0400

    Add json creator
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- learner-framework-executor/src/main/java/daee/learner/framework/dto/TrainingVariableDTO.java
+++ learner-framework-executor/src/main/java/daee/learner/framework/dto/TrainingVariableDTO.java
@@ -1,11 +1,14 @@
 package daee.learner.framework.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class TrainingVariableDTO {
 
     private String name;
     private Long id;
     private Boolean target;
 
+    @JsonCreator
     public TrainingVariableDTO(String name, Long id, Boolean target) {
         this.name = name;
         this.id = id;
